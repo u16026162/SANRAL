879c9e01891333e09bc5036a5f8e949d8f0bdab1
u16026162 <u16026162@tuks.co.za>
2023-02-13 18:02:22 +0200

    Version 2

--- docs/Van_Zylspruit_Dashboard.js
+++ docs/Van_Zylspruit_Dashboard.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['https://cdn.holoviz.org/panel/0.14.2/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.2/dist/wheels/panel-0.14.2-py3-none-any.whl', 'pyodide-http==0.1.0', 'pandas', 'numpy', 'matplotlib', 'holoviews', 'hvplot', 'datetime']
+  const env_spec = ['https://cdn.holoviz.org/panel/0.14.2/dist/wheels/bokeh-2.4.3-py3-none-any.whl', 'https://cdn.holoviz.org/panel/0.14.2/dist/wheels/panel-0.14.2-py3-none-any.whl', 'pyodide-http==0.1.0', 'pandas', 'numpy', 'matplotlib', 'holoviews', 'hvplot', 'datetime', 'fastparquet']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
